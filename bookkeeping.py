0411aa4e2ce481d3b84410bffe08a5c15a71ac84
Quei-An Chen <kwea123@gmail.com>
2018-06-30 22:08:02 +0800

    refractor

--- bookkeeping.py
+++ bookkeeping.py
@@ -10,17 +10,8 @@ import re
 # import numpy as np
 from datetime import datetime, timedelta
 import os
+from utils import *
 
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-import pyaudio
-from six.moves import queue
-
-# Audio recording parameters
-RATE = 44100
-CHUNK = int(RATE / 1000)  # 100ms
-# mixer.init(frequency=int(RATE*1.1)) # playing speed is 1.1x faster
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/kwea123/Downloads/MyProject-e85ed8c91456.json'
 
 if os.path.isfile('data.csv'):
@@ -31,70 +22,6 @@ else:
     df = pd.DataFrame(columns=['日期', '名稱', '價錢', '刪除'])
 # df = df.append({'日期': datetime.strptime('2018-06-30', "%Y-%m-%d").date(), '名稱': 'text', '價錢': 12, '刪除': '<input type="checkbox">'}, ignore_index=True)
 # df = df.append({'日期': datetime.strptime('2018-07-01', "%Y-%m-%d").date(), '名稱': 'text', '價錢': 12, '刪除': '<input type="checkbox">'}, ignore_index=True)
-
-class MicrophoneStream(object):
-    """Opens a recording stream as a generator yielding the audio chunks."""
-    def __init__(self, rate, chunk):
-        self._rate = rate
-        self._chunk = chunk
-
-        # Create a thread-safe buffer of audio data
-        self._buff = queue.Queue()
-        self.closed = True
-
-    def __enter__(self):
-        self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            # The API currently only supports 1-channel (mono) audio
-            # https://goo.gl/z757pE
-            channels=1, rate=self._rate,
-            input=True, frames_per_buffer=self._chunk,
-            # Run the audio stream asynchronously to fill the buffer object.
-            # This is necessary so that the input device's buffer doesn't
-            # overflow while the calling thread makes network requests, etc.
-            stream_callback=self._fill_buffer,
-        )
-
-        self.closed = False
-
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self._audio_stream.stop_stream()
-        self._audio_stream.close()
-        self.closed = True
-        # Signal the generator to terminate so that the client's
-        # streaming_recognize method will not block the process termination.
-        self._buff.put(None)
-        self._audio_interface.terminate()
-
-    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        """Continuously collect data from the audio stream, into the buffer."""
-        self._buff.put(in_data)
-        return None, pyaudio.paContinue
-
-    def generator(self):
-        while not self.closed:
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
-            chunk = self._buff.get()
-            if chunk is None:
-                return
-            data = [chunk]
-
-            # Now consume whatever other data's still buffered.
-            while True:
-                try:
-                    chunk = self._buff.get(block=False)
-                    if chunk is None:
-                        return
-                    data.append(chunk)
-                except queue.Empty:
-                    break
-
-            yield b''.join(data)
 
 # def speak(sentence, lang='zh-tw'):
 #     with tempfile.NamedTemporaryFile(delete=True) as fp:
